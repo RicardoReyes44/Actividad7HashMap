0ad55e1fb8f64cd7ec699dec0315e8445b56ebc1
RicardoReyes44 <rickyboy1002@gmail.com>
2020-10-01 11:20:50 -0500

    Se agrego un menu en la clase de prueba 'PruebaHashMap' y manejo de excepciones

--- src/PruebaHashMap.java
+++ src/PruebaHashMap.java
@@ -1,8 +1,59 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class PruebaHashMap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner entrada = new Scanner(System.in);
+		boolean candado = true;
+		
+		while(candado) {
+			System.out.println("----------Menu principal-----------");
+			System.out.println("1.- Llenar lista");
+			System.out.println("2.- Vaciar lista");
+			System.out.println("3.- Mostrar aumnos por carrera");
+			System.out.println("4.- Calcular promedio de edades");
+			System.out.println("5.- Mostrar los alumnos que se incribieron antes del (10/08/2016)");
+			System.out.println("6.- Salir");
+			System.out.print("Introduce opcion: ");
+			
+			try {
+				
+				byte opcion = entrada.nextByte();
+				
+				switch(opcion) {
+				case 1:
+					break;
+					
+				case 2:
+					break;
+					
+				case 3:
+					break;
+					
+				case 4:
+					break;
+					
+				case 5:
+					break;
+					
+				case 6:
+					candado = false;
+					break;
+					
+				default:
+					System.out.println("Opcion inexistente, por favor prueba de nuevo\n");
+					break;
+					
+				}
+				System.out.println();
+	
+			}catch(InputMismatchException error) {
+				System.out.println("\nError en la entrada de datos<" + error + ">, por favor pueba de nuevo\n");
+				entrada.nextLine();
+			}
+		}
 
 	}
 

5e0586f74fd639552468da30890f029b52d200b0
RicardoReyes44 <rickyboy1002@gmail.com>
2020-10-01 11:48:25 -0500

    Se agregaron setters y getters en la clase 'Alumno' y se sobreescribio el metodo 'toString'

--- src/Alumno.java
+++ src/Alumno.java
@@ -14,4 +14,35 @@ public class Alumno {
 		this.fechaInscripcion = fechaInscripcion;
 	}
 	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getCarrera() {
+		return carrera;
+	}
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+	public String getFechaInscripcion() {
+		return fechaInscripcion;
+	}
+	public void setFechaInscripcion(String fechaInscripcion) {
+		this.fechaInscripcion = fechaInscripcion;
+	}
+	
+	@Override
+	public String toString() {
+		return "Alumno [nombre=" + nombre + ", edad=" + edad + ", carrera=" + carrera + ", fechaInscripcion="
+				+ fechaInscripcion + "]";
+	}
+	
 }

0c3cec4a931ca2ced4c04876201a5b17ab9bbdd2
RicardoReyes44 <rickyboy1002@gmail.com>
2020-10-01 09:00:22 -0500

    Se agregaron los atributos 'nombre', 'edad', 'carrera', y 'fechaInscripcion' y tambien el constructor predeterminado y otro para inicializar valores a la clase 'Alumno'

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,4 +1,17 @@
 
 public class Alumno {
-
+	
+	private String nombre;
+	private int edad;
+	private String carrera;
+	private String fechaInscripcion;
+	
+	public Alumno() {}
+	public Alumno(String nombre, int edad, String carrera, String fechaInscripcion){
+		this.nombre = nombre;
+		this.edad = edad;
+		this.carrera = carrera;
+		this.fechaInscripcion = fechaInscripcion;
+	}
+	
 }

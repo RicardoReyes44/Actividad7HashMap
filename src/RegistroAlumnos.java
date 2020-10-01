96b2e38771244e95cb09f0f341ea4a4355345336
RicardoReyes44 <rickyboy1002@gmail.com>
2020-10-01 14:24:34 -0500

    Se agrego el metodo de 'seleccionarCarrera' en la clase 'RegistroAlumnos'

--- src/RegistroAlumnos.java
+++ src/RegistroAlumnos.java
@@ -8,7 +8,37 @@ public class RegistroAlumnos {
 	Scanner entrada = new Scanner(System.in);
 	
 	public RegistroAlumnos() {mapNombres = new HashMap<Integer, Alumno>();}
-	
+
+
+    public void vaciarLista() {
+		mapNombres.clear();
+	}
+
+    
+    public String seleccionarCarrera() {
+    	String carreras[] = {"ISC", "IIA", "IM", "LA", "CP"};
+    	byte opcion;
+    	
+    	while(true) {
+    		System.out.println("1.- ISC");
+    		System.out.println("2.- IIA");
+    		System.out.println("3.- IM");
+    		System.out.println("4.- LA");
+    		System.out.println("5.- CP");
+    		System.out.print("Selecciona una carrera: ");
+    		opcion = entrada.nextByte();
+    		
+    		if(opcion<=5 && opcion>=1) {
+    			break;
+    		}else {
+    			System.out.println("Opcion inexistente, por favor vuelve a intentarlo");
+    		}
+    	}
+    	
+    	return carreras[opcion-1];
+	}
+
+
 	public void limpiarMemoria(){
 		entrada.nextLine();
 	}
